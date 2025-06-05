78f784555da76f714976371eb76503427abb9dfb
fromtheboonies <fromtheboonies@users.noreply.github.com>
2025-06-05 02:07:43 +0000

    deploy: aab949f5c230078ff2521946c738515a74f9d414

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "How I Filtered 1,000+ Job Alert Emails with Python and AI",
+        "excerpt":"Job hunting gets noisy fast. I built a Python script using the Microsoft Graph API and some local LLM logic to classify and sort all my LinkedIn job alerts into folders — tagging them as review, ignore, or priority.  ","categories": ["automation","ai","job-search"],
+        "tags": [],
+        "url": "/automation/ai/job-search/2025/06/04/job-filtering.html",
+        "teaser": null
+      }]
